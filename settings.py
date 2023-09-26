da7aaefe27375809c3b2d740c1db569480443547
Lucas Fontes <lucasefontes@gmail.com>
2023-09-26 13:28:48 -0300

    v1.2

--- settings.py
+++ settings.py
@@ -7,3 +7,13 @@ class Settings:
         self.screen_width = 1000
         self.screen_height = 700
         self.bg_color = (0,0,0)
+
+        #Ship settings
+        self.ship_speed = 1.5
+
+        #bullet settings
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         """Initialize the ship and sets its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         #Load ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,24 @@ class Ship:
 
         #Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Store a float for the ship's exact horizontal position.
+        self.right = float(self.rect.right)
+
+        #Movement flag; start with a ship that's not moving.
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        #update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.right += self.settings.ship_speed
+        if self.moving_left and self.rect.left:
+            self.right -= self.settings.ship_speed
+        
+        #Update rect object from self.x.
+        self.rect.right = self.right
 
     def blitme(self):
         """Drawn the ship at its current location."""
